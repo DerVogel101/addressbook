f1c692e562d1ac56054ba060a8b1c80e102ba800
DerVogel101 <jan.irmer@outlook.de>
2024-08-11 15:56:40 +0200

    scrapped pandas switched to manuall implementation and added a unique constraint to the adresses table

--- address.py
+++ address.py
@@ -3,9 +3,6 @@ from typing import Optional
 from no_touchy.muple import Muple
 from pydantic import field_validator, PositiveInt
 from pydantic.dataclasses import dataclass
-import sqlite3
-import pandas as pd
-from contextlib import closing
 import re
 import phonenumbers
 
@@ -74,27 +71,5 @@ class Address:
                 f"{self.birthdate}\n{self.phone}\n{self.email}")
 
 
-def save_to_sqlite(addresses: list[dataclass]):
-    with closing(sqlite3.connect('addresses.sqlite3')) as conn:
-        addresses_df = pd.DataFrame([address_i.__dict__ for address_i in addresses])
-        addresses_df.to_sql('addresses', conn, if_exists='append', index=False)
-
-
 if __name__ == "__main__":
-    address = Address(lastname="ADoe", firstname="John", street="Main Street", number="123", zip_code=12345,
-                      city="Springfield", birthdate="2000-01-01", phone="+49 176 1234 5678", email="john.doe@doe-mail.io")
-
-    adress3 = Address(lastname="Abc", firstname="a")
-    adress4 = Address(lastname="Abc", firstname="b")
-    adress5 = Address(lastname="bbc", firstname="a")
-    adress6 = Address(lastname="bbc", firstname="b")
-
-    address7 = Address(lastname="bbc", firstname="b", street="Main Street", number="123", zip_code=12345)
-    print(repr(address))
-
-    sort_list = [adress4, adress6, adress3, adress5]
-    save_to_sqlite([address, adress3, adress4, adress5, adress6])
-    save_to_sqlite([address7])
-    sort_list.sort()
-    print(sort_list)
-    print(address.__dict__)
+    pass
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from pydantic import ValidationError
-
 from address import Address
-import sqlite3
-
-conn = sqlite3.connect('addresses.sqlite3')
-cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS addresses (lastname TEXT, firstname TEXT, street TEXT, number TEXT, zip_code INTEGER, city TEXT, birthdate TEXT, phone TEXT, email TEXT)")
-conn.commit()
+from sqlite_database import save_to_sqlite
 
 if __name__ == "__main__":
-    address = Address("Doe", "John", "Main Street", 123, 12345,
-                      "Springfield", "01.01.1970", "+49123456789", "john.doe@doe-mail.io")
-    print(address)
+    address = Address(lastname="ADoe", firstname="John", street="Main Street", number="123", zip_code=12345,
+                      city="Springfield", birthdate="2000-01-01", phone="+49 176 1234 5678",
+                      email="john.doe@doe-mail.io")
+
+    adress3 = Address(lastname="Abc", firstname="a")
+    adress4 = Address(lastname="Abc", firstname="b")
+    adress5 = Address(lastname="bbc", firstname="a")
+    adress6 = Address(lastname="bbc", firstname="b")
+
+    address7 = Address(lastname="bbc", firstname="b", street="Main Street", number="123", zip_code=12345)
     print(repr(address))
+
+    sort_list = [adress4, adress6, adress3, adress5]
+    save_to_sqlite([address, adress3, adress4, adress5, adress6])
+    save_to_sqlite([address7])
+    sort_list.sort()
+    print(sort_list)
+    print(address.__dict__)
 

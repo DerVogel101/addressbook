493e0e9842a62ce62e958c5fa147f8d76c7f5ac4
DerVogel101 <jan.irmer@outlook.de>
2024-09-05 15:10:11 +0200

    implementig interface for qsl lite

---
+++ sqlite_interface.py
@@ -0,0 +1,97 @@
+import os
+
+from address_container_interface import AddressDatabaseInterface
+from sqlite_database import SqliteDatabase
+from address import Address
+from datetime import date
+
+
+class SqliteInterface(AddressDatabaseInterface):
+    def __init__(self):
+        self.__sql_path: str | None = None
+        self.__squirrel_lite: SqliteDatabase | None = None
+
+    # def __enter__(self):
+    #     return self
+    #
+    # def __exit__(self, exc_type, exc_val, exc_tb):
+    #     if self.__squirrel_lite:
+    #         self.__squirrel_lite.close()
+    #         self.__squirrel_lite = None
+
+    def set_path(self, path: str) -> None:
+        """
+        :raises TypeError: if the path is invalid
+        """
+        if not isinstance(path, str):
+            raise TypeError("Path must be a string")
+        self.__sql_path = path
+
+    def open(self) -> None:
+        """
+        :raises ValueError: if the path is invalid
+        """
+        if not self.__sql_path:
+            raise ValueError("Path is not set")
+        self.__squirrel_lite = SqliteDatabase(self.__sql_path)
+
+    def save(self) -> None:
+        self.__squirrel_lite.save()
+
+    def get_all(self) -> dict[str: Address]:
+        pass
+
+    def get(self, __id: int) -> Address | None:
+        pass
+
+    def search(self, search_string: str) -> dict[str: Address]:
+        pass
+
+    def delete(self, __id: int) -> int | None:
+        """:return: the id of the deleted address if it was found, else None"""
+        pass
+
+    def update(self, __id: int, **kwargs) -> int | None:
+        """
+        :return: the id of the updated address if it was found, else None
+        :raises KeyError: if the address with the given id does not exist
+        """
+        pass
+
+    def add_address(self, address) -> int:
+        """:return: the id of the added address"""
+        if not isinstance(address, Address):
+            raise TypeError("Address must be an instance of Address")
+        row_id = self.__squirrel_lite.add(address)
+        return row_id
+
+    def get_today_birthdays(self) -> dict[int: Address]:
+        if not self.__squirrel_lite:
+            raise ValueError("Database is not open")
+        cursor = self.__squirrel_lite.get_where(f"birthdate = '{date.today()}'")
+        for row in cursor:
+            print(row)
+
+
+if __name__ == "__main__":
+    from context_support import ContextSupport
+    with ContextSupport(SqliteInterface()) as adress_book:
+        adress_book.set_path("addresses.sqlite3")
+        adress_book.open()
+        # adress_book.add_address(Address(
+        #     lastname="Günther", firstname="Harald", street="Main.cpp", number="-1", zip_code=404,
+        #     city="Gravity Falls", birthdate="1990-01-01", phone="+49 176 1234 5678",
+        #     email="anomaly@krampf.xd"
+        # ))
+        adress_book.add_address(Address(
+            lastname="Gunther", firstname="Hari", street="Main.cpp", number="-1", zip_code=404,
+            city="Gravity Falls", birthdate="2024-09-05", phone="+49 176 1234 5678",
+            email="anomaly@krampf.xd"
+        ))
+        adress_book.add_address(Address(
+            lastname="Gunther", firstname="Häri", street="Main.cpp", number="-1", zip_code=404,
+            city="Gravity Falls", birthdate="2024-09-05", phone="+49 176 1234 5678",
+            email="anomaly@krampf.xd"
+        ))
+        adress_book.get_today_birthdays()
+        # adress_book.save()

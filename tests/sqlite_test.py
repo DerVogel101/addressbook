e7a57129d19dd34ab868c7f9fc46bdc0cb529394
DerVogel101 <jan.irmer@outlook.de>
2024-09-16 23:36:57 +0200

    implemented example sqlite file for unittest and added pytest to req.txt

--- tests/sqlite_test.py
+++ tests/sqlite_test.py
@@ -5,47 +5,41 @@ from datetime import date
 
 import unittest
 import random
+import shutil
 import os
 
 
 class TestCsvInterface(unittest.TestCase):
 
     def setUp(self) -> None:
+        example_path = "ExampleSQLTest.db"
         self.seed = random.randint(0, 1000)
+        shutil.copyfile(example_path, rf"./sqlTest{self.seed}.db")
         self.interface = SqliteInterface(rf"./sqlTest{self.seed}.db")
         self.interface.open()
 
-    # def tearDown(self):
-    #     self.interface.save()
-    #     self.interface.close()
-    #     os.remove(f"./sqlTest{self.seed}.db")
+    def tearDown(self):
+        self.interface.save()
+        self.interface.close()
+        os.remove(f"./sqlTest{self.seed}.db")
 
     def test_add_address_id(self):
         a = self.interface.add_address(
-            Address(lastname="Huber", firstname="Hans", street="Obere Bahnhofstra e", number="3", zip_code=70173,
+            Address(lastname="Jürgen", firstname="Hans", street="Obere Bahnhofstraße", number="12", zip_code=70173,
                     city="Stuttgart", birthdate="1990-01-01", phone="+49 711 1234 5678",
                     email="hans.huber@exaample.de"))
         b = self.interface.add_address(
-            Address(lastname="Schmidt", firstname="J rgen", street="K nigstra e", number="1", zip_code=70173,
+            Address(lastname="Müller", firstname="Jürgen", street="Königstraße", number="3", zip_code=70173,
                     city="Stuttgart", birthdate="1980-02-02", phone="+49 711 5678 9012",
                     email="juergen.schmidt@example.de"))
         c = self.interface.add_address(
-            Address(lastname="Fischer", firstname="Monika", street="Marienplatz", number="2", zip_code=70173,
+            Address(lastname="Angler", firstname="Monika", street="Marienplatz", number="6", zip_code=70173,
                     city="Stuttgart", birthdate="1970-03-03", phone="+49 711 1234 5679",
                     email="monika.fischer@example.com"))
-        d = self.interface.add_address(
-            Address(lastname="TodaysBirthday", firstname="John", street="Today Street", number="1", zip_code=12345,
-                    city="Today City", birthdate=date.today(), phone="+49 176 1234 5678",
-                    email="john.doe@example.com"))
-        import doctest
-        # assert a == 1
-        # assert b == 2
-        # assert c == 3
-        # assert d == 4
-        self.assertEqual(a, 1)
-        self.assertEqual(b, 2)
-        self.assertEqual(c, 3)
-        self.assertEqual(d, 4)
+
+        self.assertEqual(a, 4)
+        self.assertEqual(b, 5)
+        self.assertEqual(c, 6)
 
     def test_expect_all(self):
         result = self.interface.get_all()
